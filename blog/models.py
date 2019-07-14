8895d1a4f5742e0c71aa2bb8947a74588c2dd19f
Daniel Butler <daniel@dbutlercpa.com>
2019-07-14 16:00:37 -0400

    base model setup

--- blog/models.py
+++ blog/models.py
@@ -5,19 +5,6 @@ from django.db import models
 User = get_user_model()
 
 
-class EntryBody(models.Model):
-    id = models.AutoField(unique=True, primary_key=True)
-    body = models.TextField()
-    created = models.DateTimeField(auto_now_add=True, editable=False)
-    modified = models.DateTimeField(auto_now=True, editable=False)
-
-    def __str__(self):
-        return self.body[:50] + '...'
-
-    class Meta:
-        verbose_name_plural = 'entry bodies'
-
-
 class Tag(models.Model):
     id = models.AutoField(unique=True, primary_key=True)
     name = models.CharField(unique=True, max_length=200)
@@ -38,7 +25,6 @@ class Entry(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='Entries',
     )
-    body = models.ForeignKey(to=EntryBody, on_delete=models.CASCADE, related_name='Entries')
     created = models.DateTimeField(auto_now_add=True, editable=False)
     modified = models.DateTimeField(auto_now=True, editable=False)
 
@@ -49,10 +35,24 @@ class Entry(models.Model):
         verbose_name_plural = 'entries'
 
 
+class EntryBody(models.Model):
+    id = models.AutoField(unique=True, primary_key=True)
+    entry = models.ForeignKey(to=Entry, on_delete=models.CASCADE, related_name='Entries')
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.body[:50] + '...'
+
+    class Meta:
+        verbose_name_plural = 'entry bodies'
+
+
 class TagToEntry(models.Model):
     id = models.AutoField(unique=True, primary_key=True)
-    entry_id = models.ForeignKey(to=Entry, on_delete=models.CASCADE, related_name='entries')
-    tag_id = models.ForeignKey(to=Tag, on_delete=models.CASCADE, related_name='tags')
+    entry = models.ForeignKey(to=Entry, on_delete=models.CASCADE, related_name='tagged_entries')
+    tag = models.ForeignKey(to=Tag, on_delete=models.CASCADE, related_name='tagged_to_entries')
     created = models.DateTimeField(auto_now_add=True, editable=False)
     modified = models.DateTimeField(auto_now=True, editable=False)
 
--- comment/models.py
+++ comment/models.py
@@ -10,12 +10,17 @@ class Comment(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False)
     modified = models.DateTimeField(auto_now=True, editable=False)
     author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='users')
-    entry = models.ForeignKey(to=Entry, on_delete=models.CASCADE, related_name='entires')
-    reply_to = models.ForeignKey(to=Comment, on_delete=models.CASCADE, related_name='comments', blank=True, null=True)
+    entry = models.ForeignKey(to=Entry, on_delete=models.CASCADE, related_name='entries')
+
+
+class ReplyToComment(models.Model):
+    id = models.AutoField(unique=True, primary_key=True)
+    base_comment = models.ForeignKey(to=Comment, on_delete=models.CASCADE, related_name='base_comment')
+    reply_to_comment = models.ForeignKey(to=Comment, on_delete=models.CASCADE, related_name='reply_to_comments')
 
 
 class CommentBody(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False)
     modified = models.DateTimeField(auto_now=True, editable=False)
     body = models.TextField()
-    comment = models.ForeignKey(to=Comment, on_delete=models.CASCADE, related_name='comments')
+    comment = models.ForeignKey(to=Comment, on_delete=models.CASCADE, related_name='comment')
