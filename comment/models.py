93e1d3afc243dbd7460b132705db9ea9507999ab
Daniel Butler <daniel@dbutlercpa.com>
2019-07-14 11:19:11 -0400

    base configuration for comment models

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from blog.models import Entry
+
+User = get_user_model()
+
+
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True, editable=False)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='users')
+    entry = models.ForeignKey(to=Entry, on_delete=models.CASCADE, related_name='entires')
+    reply_to = models.ForeignKey(to=Comment, on_delete=models.CASCADE, related_name='comments', blank=True, null=True)
+
+
+class CommentBody(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True, editable=False)
+    body = models.TextField()
+    comment = models.ForeignKey(to=Comment, on_delete=models.CASCADE, related_name='comments')
