8f123701ee603b401eb0ff2e2bc84bf51f96d095
Daniel Butler <daniel@dbutlercpa.com>
2019-07-14 23:15:41 -0400

    displaying blog entries

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Entry
+
+
+def homepage(request):
+    entries = Entry.objects.all()
+    return render(request, template_name='pages/home.html', context={'entries': entries})
